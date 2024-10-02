90e5626373e9c7367dd1429bd9692f343e2197c8
Ali <ali3abbasov@gmail.com>
2024-10-02 20:33:01 +0400

    refactor: Updated password brute forcer

--- 5 - Brute-force Password Cracker/app.py
+++ 5 - Brute-force Password Cracker/app.py
@@ -3,21 +3,20 @@ import hashlib
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
-def brute_force(input_password):
+def brute_force(hashed_input_password):
     with open('1M_passwords.txt', 'r') as file:
         wordlist = file.read().splitlines()
         print(wordlist)
 
         for word in wordlist:
-            hashed_input_password = hash_password(input_password)
-            hashed_word = hash_password(word)
+            hashed_word = hash_password(word)  # Hash each word in the wordlist
             if hashed_input_password == hashed_word:
                 print(f"Password found: {word}")
                 return
-            else :
+            else:
                 print(f"Password not found: {word}")
         print("Password not found in wordlist.")
 
 if __name__ == "__main__":
-    password_hash = input("Enter password: ")
-    brute_force(password_hash)
+    hashed_password = input("Enter hashed password: ")
+    brute_force(hashed_password)
